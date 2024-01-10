d7c0d461d47c78c306ab3f97c336324545e6b94c
Maryll Castelino <maryllcastelino@gmail.com>
2024-01-10 22:33:16 +0530

    upload to yt final update

--- src/pmg/instatoyt/views.py
+++ src/pmg/instatoyt/views.py
@@ -1,9 +1,12 @@
 # views.py
+import os
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Reel
 from .serializers import ReelSerializer
 from instaloader import Instaloader, Post
+import shutil
+from .uploadtoyt import upload_youtube_video
 
 class ReelViewSet(viewsets.ModelViewSet):
     queryset = Reel.objects.all()
@@ -14,9 +17,45 @@ class ReelViewSet(viewsets.ModelViewSet):
         # Extract the string parameter from the request data
         reel_id = request.data.get('reel_id', '')
         print(reel_id)
-        
+
+
+        # os.chdir('instatoyt')
+        # print(os.getcwd())
+
+        # Delete the entire folder
+        try:
+            shutil.rmtree('current_reel')
+            print(f"The folder current_reel has been deleted.")
+        except Exception as e:
+            print(f"Failed to delete the folder current_reel. Reason: {e}")
         self.downloadReel(reel_id)
-        # self.uploadToYT()
+
+        # # Path to the directory containing MP4 files
+        # target_dir = "current_reel"
+
+        # # Get a list of all files in the directory
+        # all_files = os.listdir(target_dir)
+
+        file_path = "current_reel/reels.mp4"
+        title_path = "current_reel/reels.txt"
+        title = ""
+        description = ""
+        if os.path.isfile(title_path):
+            try:
+                with open(title_path, 'r') as file:
+                    title = file.readline()
+                    description = file.read()
+                    print(f"The first line of the file is: {title}")
+            except FileNotFoundError:
+                print(f"Error: File '{title_path}' not found.")
+            except IOError as e:
+                print(f"Error reading the file: {e}")
+
+        # title = "My Uploaded Video 2"
+        category = "22"
+        keywords = "keyword1,keyword2"
+        privacy_status = "private"
+        upload_youtube_video(file_path, title, description, category, keywords, privacy_status)
 
         # Create a new instance of Reel with the provided string parameter
         instance = Reel(**request.data)
@@ -28,9 +67,7 @@ class ReelViewSet(viewsets.ModelViewSet):
     
     def downloadReel(self, SHORTCODE):
         # Get instance
-        L = Instaloader()
+        L = Instaloader(filename_pattern="reels")
         # You can perform additional validation or processing here
         post = Post.from_shortcode(L.context, SHORTCODE)
         L.download_post(post, target='current_reel')
-
-        # self.uploadToYT()
